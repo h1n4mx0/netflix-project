b0aa9eeff9049e16893b7fa8c003170a107e1629
h1n4mx0 <116544941+h1n4mx0@users.noreply.github.com>
2025-06-03 17:38:51 -0700

    Improve base layout with gradient background

--- frontend/anieflix/src/layouts/Layout.jsx
+++ frontend/anieflix/src/layouts/Layout.jsx
@@ -3,12 +3,12 @@ import Navbar from '../components/Navbar'
 
 export default function Layout() {
   return (
-    <div className="bg-black min-h-screen text-white">
+    <div className="bg-gradient-to-br from-[#141E30] to-[#243B55] min-h-screen text-white flex flex-col">
       {/* Navbar cố định trên cùng */}
       <Navbar />
 
       {/* Phần nội dung chính của mỗi trang */}
-      <main className="">
+      <main className="flex-1 pt-14 container mx-auto px-4 sm:px-8">
         <Outlet />
       </main>
     </div>
